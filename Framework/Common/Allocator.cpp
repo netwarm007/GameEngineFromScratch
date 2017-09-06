4cec91f9e0a0668083f853053cad52f44b9e8854
Chen Wenli <chenwenli@chenwenli.com>
2017-09-06 16:52:39 +0800

    in the middle of article_19

--- Framework/Common/Allocator.cpp
+++ Framework/Common/Allocator.cpp
@@ -8,6 +8,13 @@
 
 using namespace My;
 
+My::Allocator::Allocator()
+        : m_szDataSize(0), m_szPageSize(0), 
+        m_szBlockSize(0), m_szAlignmentSize(0), m_nBlocksPerPage(0),
+        m_pPageList(nullptr), m_pFreeList(nullptr)
+{
+}
+
 My::Allocator::Allocator(size_t data_size, size_t page_size, size_t alignment)
         : m_pPageList(nullptr), m_pFreeList(nullptr)
 {
--- Framework/Common/MemoryManager.hpp
+++ Framework/Common/MemoryManager.hpp
@@ -1,11 +1,38 @@
 #pragma once
 #include "IRuntimeModule.hpp"
+#include "Allocator.hpp"
+#include <new>
 
 namespace My {
+    template<typename T, typename... Arguments>
+    T* New(Arguments... parameters)
+    {
+        return new (Allocate(sizeof(T))) T(Arguments);
+    }
+
+    template<typename T>
+    void Delete(T *p)
+    {
+        reinterpret_cast<T*>(p)->~T();
+        Free(p, sizeof(T));
+    }
+
 	class MemoryManager : implements IRuntimeModule
 	{
 	public:
 		virtual ~MemoryManager() {}
+
+        virtual int Initialize();
+        virtual void Finalize();
+        virtual void Tick();
+
+        void* Allocate(size_t size);
+        void  Free(void* p, size_t size);
+    private:
+        static size_t*        m_pBlockSizeLookup;
+        static Allocator*     m_pAllocators;
+    private:
+        static Allocator* LookUpAllocator(size_t size);
 	};
 }
 
