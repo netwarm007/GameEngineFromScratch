818f1162452d5f657bf56b4519200e481e05558f
Chen, Wenli <chenwenli@chenwenli.com>
2017-09-15 23:37:26 +0800

    merge OpenGL glx. Not tested

--- Platform/Linux/XcbApplication.cpp
+++ Platform/Linux/XcbApplication.cpp
@@ -5,18 +5,11 @@
 
 using namespace My;
 
-namespace My {
-    GfxConfiguration config(8, 8, 8, 8, 32, 0, 0, 960, 540, "Game Engine From Scratch (XCB)");
-    IApplication* g_pApp                  = static_cast<IApplication*>(new XcbApplication(config));
-    GraphicsManager* g_pGraphicsManager   = static_cast<GraphicsManager*>(new GraphicsManager);
-    MemoryManager*   g_pMemoryManager     = static_cast<MemoryManager*>(new MemoryManager);
-}
-
 int My::XcbApplication::Initialize()
 {
     int result;
-  	uint32_t		mask = 0;
-	uint32_t		values[2];
+    uint32_t        mask = 0;
+    uint32_t        values[2];
 
     // first call base class initialization
     result = BaseApplication::Initialize();
@@ -24,48 +17,65 @@ int My::XcbApplication::Initialize()
     if (result != 0)
         exit(result);
 
-	/* establish connection to X server */
-	m_pConn = xcb_connect(0, 0);
+    if (!m_pConn) {
+        /* establish connection to X server */
+        m_pConn = xcb_connect(0, 0);
+    }
 
-	/* get the first screen */
-	m_pScreen = xcb_setup_roots_iterator(xcb_get_setup(m_pConn)).data;
+    if (!m_pScreen) {
+        /* get the first screen */
+        m_pScreen = xcb_setup_roots_iterator(xcb_get_setup(m_pConn)).data;
+        m_nVi = m_pScreen->root_visual;
+    }
 
-	/* get the root window */
-	m_Window = m_pScreen->root;
+    /* get the root window */
+    m_Window = m_pScreen->root;
 
-	/* create window */
-	m_Window = xcb_generate_id(m_pConn);
-	mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-	values[0] = m_pScreen->white_pixel;
-	values[1] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
-	xcb_create_window (m_pConn,					/* connection */
-					   XCB_COPY_FROM_PARENT,	/* depth */
-					   m_Window,					/* window ID */
-					   m_pScreen->root,			/* parent window */
-					   20, 20,					/* x, y */
-					   m_Config.screenWidth,    /* width */
-                       m_Config.screenHeight,	/* height */
-					   10,						/* boarder width */
-					   XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class */
-					   m_pScreen->root_visual,	/* visual */
-					   mask, values);			/* masks */
+    /* Create XID's for colormap */
+    xcb_colormap_t colormap = xcb_generate_id(m_pConn);
 
-	/* set the title of the window */
-	xcb_change_property(m_pConn, XCB_PROP_MODE_REPLACE, m_Window,
-			    XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8,
-			    strlen(m_Config.appName), m_Config.appName);
+    xcb_create_colormap(
+        m_pConn,
+        XCB_COLORMAP_ALLOC_NONE,
+        colormap,
+        m_Window,
+        m_nVi 
+        );
 
-	/* set the title of the window icon */
+    /* create window */
+    m_Window = xcb_generate_id(m_pConn);
+    mask = XCB_CW_EVENT_MASK  | XCB_CW_COLORMAP;
+    values[0] = XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS;
+    values[1] = colormap;
+    values[2] = 0;
+    xcb_create_window (m_pConn,                 /* connection */
+                       XCB_COPY_FROM_PARENT,    /* depth */
+                       m_Window,                /* window ID */
+                       m_pScreen->root,         /* parent window */
+                       20, 20,                  /* x, y */
+                       m_Config.screenWidth,    /* width */
+                       m_Config.screenHeight,   /* height */
+                       10,                      /* boarder width */
+                       XCB_WINDOW_CLASS_INPUT_OUTPUT, /* class */
+                       m_nVi,  /* visual */
+                       mask, values);           /* masks */
+
+    /* set the title of the window */
+    xcb_change_property(m_pConn, XCB_PROP_MODE_REPLACE, m_Window,
+                XCB_ATOM_WM_NAME, XCB_ATOM_STRING, 8,
+                strlen(m_Config.appName), m_Config.appName);
+
+    /* set the title of the window icon */
     /*
-	xcb_change_property(m_pConn, XCB_PROP_MODE_REPLACE, m_Window,
-			    XCB_ATOM_WM_ICON_NAME, XCB_ATOM_STRING, 8,
-			    strlen(m_Config.appName), m_Config.appName);
+    xcb_change_property(m_pConn, XCB_PROP_MODE_REPLACE, m_Window,
+                XCB_ATOM_WM_ICON_NAME, XCB_ATOM_STRING, 8,
+                strlen(m_Config.appName), m_Config.appName);
     */
 
-	/* map the window on the screen */
-	xcb_map_window(m_pConn, m_Window);
+    /* map the window on the screen */
+    xcb_map_window(m_pConn, m_Window);
 
-	xcb_flush(m_pConn);
+    xcb_flush(m_pConn);
 
     return result;
 }
@@ -80,15 +90,15 @@ void My::XcbApplication::Tick()
     xcb_generic_event_t* pEvent;
     pEvent = xcb_wait_for_event(m_pConn);
     switch(pEvent->response_type & ~0x80) {
-	case XCB_EXPOSE:
-		    {		
-		    }
-			break;
-	case XCB_KEY_PRESS:
+    case XCB_EXPOSE:
+            {       
+            }
+            break;
+    case XCB_KEY_PRESS:
             BaseApplication::m_bQuit = true;
-			break;
-	}
-	free(pEvent);
+            break;
+    }
+    free(pEvent);
 }
 
 
