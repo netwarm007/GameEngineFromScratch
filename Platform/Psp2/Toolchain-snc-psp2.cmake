969e04d46b3daa515d1ac788836a00b0e89b35f6
Chen Wenli <chenwenli@chenwenli.com>
2017-09-28 17:01:34 +0800

    in the middle of PSP2 porting

---
+++ Platform/Psp2/Toolchain-snc-psp2.cmake
@@ -0,0 +1,21 @@
+# this one is important
+SET(CMAKE_SYSTEM_NAME Linux)
+#this one not so much
+SET(CMAKE_SYSTEM_VERSION 3.570)
+
+SET(SNC_TOOLCHAIN_DIR "C:/Program Files (x86)/SCE/PSP2 SDKs/3.570/host_tools/build/bin/")
+SET(SNC_TARGET_ROOT_DIR "C:/Program Files (x86)/SCE/PSP2 SDKs/3.570/target")
+
+# specify the cross compiler
+SET(CMAKE_C_COMPILER ${SNC_TOOLCHAIN_DIR}psp2snc.exe)
+SET(CMAKE_CXX_COMPILER ${SNC_TOOLCHAIN_DIR}psp2snc.exe)
+SET(CMAKE_AR ${SNC_TOOLCHAIN_DIR}psp2snarl.exe CACHE INTERNAL "Archieve")
+
+# where is the target environment 
+SET(CMAKE_FIND_ROOT_PATH  ${SNC_TARGET_ROOT_DIR})
+
+# search for programs in the build host directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+# for libraries and headers in the target directories
+SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
