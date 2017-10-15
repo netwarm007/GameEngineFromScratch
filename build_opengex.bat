00b8ce77cc522267570d0ea36ff3025b18c9bdbe
Chen Wenli <chenwenli@chenwenli.com>
2017-10-15 12:23:37 +0800

    fix build script error

--- build_opengex.bat
+++ build_opengex.bat
@@ -2,6 +2,6 @@
 git submodule update --init External/src/opengex
 mkdir -p External\build\opengex
 cd External\build\opengex
-cmake -DCMAKE_INSTALL_PREFIX=../../Windows ../..External//src/opengex
+cmake -DCMAKE_INSTALL_PREFIX=../../Windows ../../src/opengex
 cmake --build . --config release --target install
 
