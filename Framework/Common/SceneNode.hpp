6c2ef9a70b58dba98076c52eab7439e851a007a6
Chen Wenli <chenwenli@chenwenli.com>
2017-10-25 16:29:09 +0800

    continue to implement scene node

--- Framework/Common/SceneNode.hpp
+++ Framework/Common/SceneNode.hpp
@@ -6,16 +6,36 @@
 #include "SceneObject.hpp"
 
 namespace My {
-    struct BaseSceneNode {
-        std::string name;
-        std::list<std::unique_ptr<BaseSceneNode>> children;
+    class BaseSceneNode {
+        protected:
+            std::string m_strName;
+            std::list<std::unique_ptr<BaseSceneNode>> m_Children;
+            std::vector<std::unique_ptr<SceneObjectTransform>> m_Transforms;
 
-        std::vector<std::unique_ptr<SceneObjectTransform>> transforms;
+        public:
+            BaseSceneNode() {};
+            BaseSceneNode(const char* name) { m_strName = name; };
+            BaseSceneNode(const std::string& name) { m_strName = name; };
+            BaseSceneNode(const std::string&& name) { m_strName = std::move(name); };
+
+            void AppendChild(std::unique_ptr<BaseSceneNode>&& sub_node)
+            {
+                m_Children.push_back(std::move(sub_node));
+            }
+
+            void AppendChild(std::unique_ptr<SceneObjectTransform>&& transform)
+            {
+                m_Transforms.push_back(std::move(transform));
+            }
     };
 
     template <typename T>
-    struct SceneNode : public BaseSceneNode {
-        std::shared_ptr<T> pSceneObject;
+    class SceneNode : public BaseSceneNode {
+        protected:
+            std::shared_ptr<T> m_pSceneObject;
+
+        public:
+            using BaseSceneNode::BaseSceneNode;
     };
 
     typedef BaseSceneNode SceneEmptyNode;
@@ -27,6 +47,14 @@ namespace My {
             bool        m_bMotionBlur;
 
         public:
+            using SceneNode::SceneNode;
+
+            void SetVisibility(bool visible) { m_bVisible = visible; };
+            const bool Visible() { return m_bVisible; };
+            void SetIfCastShadow(bool shadow) { m_bShadow = shadow; };
+            const bool CastShadow() { return m_bShadow; };
+            void SetIfMotionBlur(bool motion_blur) { m_bMotionBlur = motion_blur; };
+            const bool MotionBlur() { return m_bMotionBlur; };
     };
 
     class SceneLightNode : public SceneNode<SceneObjectLight> 
@@ -35,6 +63,10 @@ namespace My {
             Vector3f m_Target;
 
         public:
+            using SceneNode::SceneNode;
+
+            void SetTarget(Vector3f& target) { m_Target = target; };
+            const Vector3f& GetTarget() { return m_Target; };
     };
 
     class SceneCameraNode : public SceneNode<SceneObjectCamera>
@@ -43,6 +75,10 @@ namespace My {
             Vector3f m_Target;
 
         public:
+            using SceneNode::SceneNode;
+
+            void SetTarget(Vector3f& target) { m_Target = target; };
+            const Vector3f& GetTarget() { return m_Target; };
     };
 }
 
