1576f5a93bbfc4b2856f2fc0c0633a3dadee55f4
Chen Wenli <chenwenli@chenwenli.com>
2017-10-29 21:07:54 +0800

    small adjustment of the dump function of SceneNode

--- Framework/Common/SceneNode.hpp
+++ Framework/Common/SceneNode.hpp
@@ -63,13 +63,21 @@ namespace My {
         protected:
             std::shared_ptr<T> m_pSceneObject;
 
+        protected:
+            virtual void dump(std::ostream& out) const 
+            { 
+                if ( m_pSceneObject)
+                    out << *m_pSceneObject << std::endl;
+            };
+
         public:
             using BaseSceneNode::BaseSceneNode;
-            SceneNode() { m_pSceneObject = std::make_shared<T>(); };
+            SceneNode() = default;
             SceneNode(const std::shared_ptr<T>& object) { m_pSceneObject = object; };
             SceneNode(const std::shared_ptr<T>&& object) { m_pSceneObject = std::move(object); };
 
             void AddSceneObjectRef(const std::shared_ptr<T>& object) { m_pSceneObject = object; };
+            void AddSceneObjectRef(const std::shared_ptr<T>&& object) { m_pSceneObject = std::move(object); };
 
     };
 
@@ -80,15 +88,19 @@ namespace My {
             bool        m_bVisible;
             bool        m_bShadow;
             bool        m_bMotionBlur;
+            std::vector<std::shared_ptr<SceneObjectMaterial>> m_Materials;
 
         protected:
             virtual void dump(std::ostream& out) const 
             { 
-                if (m_pSceneObject)
-                    out << *m_pSceneObject << std::endl;
+                SceneNode::dump(out);
                 out << "Visible: " << m_bVisible << std::endl;
                 out << "Shadow: " << m_bShadow << std::endl;
                 out << "Motion Blur: " << m_bMotionBlur << std::endl;
+                out << "Material(s): " << std::endl;
+                for (auto material : m_Materials) {
+                    out << *material << std::endl;
+                }
             };
 
         public:
@@ -100,6 +112,8 @@ namespace My {
             const bool CastShadow() { return m_bShadow; };
             void SetIfMotionBlur(bool motion_blur) { m_bMotionBlur = motion_blur; };
             const bool MotionBlur() { return m_bMotionBlur; };
+            using SceneNode::AddSceneObjectRef;
+            void AddSceneObjectRef(const std::shared_ptr<SceneObjectMaterial>& object) { m_Materials.push_back(object); };
     };
 
     class SceneLightNode : public SceneNode<SceneObjectLight> 
@@ -109,7 +123,6 @@ namespace My {
 
         public:
             using SceneNode::SceneNode;
-            SceneLightNode():SceneNode(std::make_shared<SceneObjectOmniLight>()){};
 
             void SetTarget(Vector3f& target) { m_Target = target; };
             const Vector3f& GetTarget() { return m_Target; };
@@ -122,7 +135,6 @@ namespace My {
 
         public:
             using SceneNode::SceneNode;
-            SceneCameraNode():SceneNode(std::make_shared<SceneObjectPerspectiveCamera>()){};
 
             void SetTarget(Vector3f& target) { m_Target = target; };
             const Vector3f& GetTarget() { return m_Target; };
