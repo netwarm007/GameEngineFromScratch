98b8254e28f3aa8a4a1b88ef24e6637a6606507b
Chen Wenli <chenwenli@chenwenli.com>
2017-10-31 22:21:58 +0800

    fix linux build error and remove dependency on External source tree

--- build_crossguid.sh
+++ build_crossguid.sh
@@ -2,6 +2,6 @@
 git submodule update --init External/src/crossguid
 mkdir -p External/build/crossguid
 cd External/build/crossguid
-cmake -DCMAKE_INSTALL_PREFIX=../../Linux -DXG_TESTS=OFF ../../src/crossguid
-cmake --build . --config release --target install
+cmake -DCMAKE_INSTALL_PREFIX=../../Linux ../../src/crossguid
+cmake --build . --config debug --target install
 
