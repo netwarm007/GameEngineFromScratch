593fecd88eebc45347ca575a4af7ecd9798b6189
Chen Wenli <chenwenli@chenwenli.com>
2017-11-16 22:38:29 +0800

    start of article_32, link scene manager and graphics manager

--- Framework/Common/SceneManager.cpp
+++ Framework/Common/SceneManager.cpp
@@ -23,13 +23,62 @@ void SceneManager::Tick()
 {
 }
 
-void SceneManager::LoadOgexScene(const char* scene_file_name)
+void SceneManager::LoadScene(const char* scene_file_name)
+{
+    // now we only has ogex scene parser, call it directly
+    LoadOgexScene(scene_file_name);
+}
+
+void SceneManager::LoadOgexScene(const char* ogex_scene_file_name)
 {
     AssetLoader asset_loader;
-    string ogex_text = asset_loader.SyncOpenAndReadTextFileToString(scene_file_name);
+    string ogex_text = asset_loader.SyncOpenAndReadTextFileToString(ogex_scene_file_name);
 
     OgexParser ogex_parser;
     m_RootNode = ogex_parser.Parse(ogex_text);
 }
 
+const shared_ptr<SceneObjectCamera> SceneManager::GetFirstCamera()
+{
+    return m_Cameras.front();
+}
+
+const shared_ptr<SceneObjectCamera> SceneManager::GetNextCamera()
+{
+    static thread_local decltype(m_Cameras.cbegin()) _it = m_Cameras.cbegin();
+    return ((_it == m_Cameras.cend()) ? *++_it : nullptr);
+}
+
+const shared_ptr<SceneObjectLight> SceneManager::GetFirstLight()
+{
+    return m_Lights.front();
+}
+
+const shared_ptr<SceneObjectLight> SceneManager::GetNextLight()
+{
+    static thread_local decltype(m_Lights.cbegin()) _it = m_Lights.cbegin();
+    return ((_it == m_Lights.cend()) ? *++_it : nullptr);
+}
+
+const shared_ptr<SceneObjectMaterial> SceneManager::GetFirstMaterial()
+{
+    return m_Materials.front();
+}
+
+const shared_ptr<SceneObjectMaterial> SceneManager::GetNextMaterial()
+{
+    static thread_local decltype(m_Materials.cbegin()) _it = m_Materials.cbegin();
+    return ((_it == m_Materials.cend()) ? *++_it : nullptr);
+}
+
+const shared_ptr<SceneObjectGeometry> SceneManager::GetFirstGeometry()
+{
+    return m_Geometries.front();
+}
+
+const shared_ptr<SceneObjectGeometry> SceneManager::GetNextGeometry()
+{
+    static thread_local decltype(m_Geometries.cbegin()) _it = m_Geometries.cbegin();
+    return ((_it == m_Geometries.cend()) ? *++_it : nullptr);
+}
 
--- Framework/Common/SceneManager.hpp
+++ Framework/Common/SceneManager.hpp
@@ -2,6 +2,7 @@
 #include "geommath.hpp"
 #include "IRuntimeModule.hpp"
 #include "SceneNode.hpp"
+#include <forward_list>
 
 namespace My {
     class SceneManager : implements IRuntimeModule
@@ -14,10 +15,25 @@ namespace My {
 
         virtual void Tick();
 
-        void LoadOgexScene(const char* scene_file_name);
+        void LoadScene(const char* scene_file_name);
+        const std::shared_ptr<SceneObjectCamera> GetFirstCamera();
+        const std::shared_ptr<SceneObjectCamera> GetNextCamera();
+        const std::shared_ptr<SceneObjectLight> GetFirstLight();
+        const std::shared_ptr<SceneObjectLight> GetNextLight();
+        const std::shared_ptr<SceneObjectMaterial> GetFirstMaterial();
+        const std::shared_ptr<SceneObjectMaterial> GetNextMaterial();
+        const std::shared_ptr<SceneObjectGeometry> GetFirstGeometry();
+        const std::shared_ptr<SceneObjectGeometry> GetNextGeometry();
 
     protected:
-        std::unique_ptr<BaseSceneNode> m_RootNode;
+        void LoadOgexScene(const char* ogex_scene_file_name);
+
+    protected:
+        std::unique_ptr<BaseSceneNode>          m_RootNode;
+        std::forward_list<std::shared_ptr<SceneObjectCamera>>      m_Cameras;
+        std::forward_list<std::shared_ptr<SceneObjectLight>>       m_Lights;
+        std::forward_list<std::shared_ptr<SceneObjectMaterial>>    m_Materials;
+        std::forward_list<std::shared_ptr<SceneObjectGeometry>>    m_Geometries;
     };
 
     extern SceneManager*    g_pSceneManager;
