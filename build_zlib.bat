721c33545f290403b0e4d8863307d5e764298dd8
tim03 <chenwenli@chenwenli.com>
2017-12-29 14:07:25 +0800

    update libraries and build scripts

--- build_zlib.bat
+++ build_zlib.bat
@@ -1,7 +1,8 @@
 @echo off
 git submodule update --init External/src/zlib
 mkdir -p External\build\zlib
-cd External\build\zlib
+pushd External\build\zlib
 cmake -DCMAKE_INSTALL_PREFIX=../Windows ../../../External/src/zlib
 cmake --build . --config release --target install
+popd
 
