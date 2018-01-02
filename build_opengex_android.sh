98cd437a9e2c0e28d135058d838a18c2e185c687
Tim <chenwenli@chenwenli.com>
2018-01-02 15:53:18 +0800

    build all things with gradlew@Android

--- build_opengex_android.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git submodule update --init External/src/opengex
-mkdir -p External/build/opengex
-pushd External/build/opengex
-cmake -DCMAKE_TOOLCHAIN_FILE=../../../cmake/android.cmake -DCMAKE_INSTALL_PREFIX=../../ ../../src/opengex
-cmake --build . --config release --target install
-popd
-
--- build_zlib_android.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git submodule update --init External/src/zlib
-mkdir -p External/build/zlib
-cd External/build/zlib
-cmake -DCMAKE_TOOLCHAIN_FILE=../../../cmake/android.cmake -DCMAKE_INSTALL_PREFIX=../../ ../../src/zlib
-cmake --build . --config release --target install
-
