2d3e91b1006945600ac27bdbaed1010fd3825b63
Chen Wenli <chenwenli@chenwenli.com>
2018-01-11 10:55:59 +0800

    update external libraries

--- build_zlib.bat
+++ build_zlib.bat
@@ -4,6 +4,6 @@ mkdir External\build\zlib
 pushd External\build\zlib
 rm -rf *
 cmake -DCMAKE_INSTALL_PREFIX=../../ -G "Visual Studio 15 2017 Win64" -Thost=x64 ../../src/zlib
-cmake --build . --target install
+cmake --build . --config release --target install
 popd
 
