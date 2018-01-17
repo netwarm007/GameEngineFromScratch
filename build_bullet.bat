4ef073def66b1f4ecbbc05ff67a0a93327d28cc8
Chen Wenli <chenwenli@chenwenli.com>
2018-01-17 13:55:36 +0800

    change bullet windows build default config from debug to release
    Signed-off-by: Chen Wenli <chenwenli@chenwenli.com>

--- build_bullet.bat
+++ build_bullet.bat
@@ -3,8 +3,8 @@ git submodule update --init External\src\bullet
 mkdir External\build\bullet
 pushd External\build\bullet
 rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=..\..\Windows\ -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017 Win64" -Thost=x64 ..\..\src\bullet
-cmake --build . --target install
+cmake -DCMAKE_INSTALL_PREFIX=..\..\Windows\ -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=RELEASE -G "Visual Studio 15 2017 Win64" -Thost=x64 ..\..\src\bullet
+cmake --build . --config release --target install
 popd
 
 
