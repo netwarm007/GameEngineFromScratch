2d5684c02b34f2e95057c7bc0b845a90421b0c38
Chen Wenli <chenwenli@chenwenli.com>
2018-01-17 13:27:02 +0800

    now can build on windows

--- build_bullet.bat
+++ build_bullet.bat
@@ -1,10 +1,10 @@
 @echo off
-git submodule update --init External/src/bullet
+git submodule update --init External\src\bullet
 mkdir External\build\bullet
 pushd External\build\bullet
 rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=../../ -G "Visual Studio 15 2017 Win64" -Thost=x64 ../../src/bullet
-cmake --build . --config debug --target install
+cmake -DCMAKE_INSTALL_PREFIX=..\..\Windows\ -DINSTALL_LIBS=ON -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=Release -G "Visual Studio 15 2017 Win64" -Thost=x64 ..\..\src\bullet
+cmake --build . --target install
 popd
 
 
