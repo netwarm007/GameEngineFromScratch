b1cff1c246d41ef7a4228c40983375223bee0d89
Tim <chenwenli@chenwenli.com>
2018-01-17 22:33:47 +0800

    fix circleci build error

--- build_bullet.sh
+++ build_bullet.sh
@@ -2,10 +2,9 @@
 
 git submodule update --init External/src/bullet
 mkdir -p External/build/bullet
-pushd External/build/bullet
+cd External/build/bullet
 rm -rf *
 cmake -DCMAKE_INSTALL_PREFIX=../../`uname -s`/ -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=RELEASE ../../src/bullet || exit 1
 cmake --build . --config release --target install
 echo "Completed build of Bullet."
-popd
 
