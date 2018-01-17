36b74477b214ba2cf5e45e3c670ead8f6bf6c39d
Chen Wenli <chenwenli@chenwenli.com>
2018-01-17 10:26:58 +0800

    add bullet test cases

--- build_bullet.sh
+++ build_bullet.sh
@@ -4,9 +4,8 @@ git submodule update --init External/src/bullet
 mkdir -p External/build/bullet
 pushd External/build/bullet
 rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=../../`uname -s`/ -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=Release ../../src/bullet || exit 1
-make -j $(command nproc 2>/dev/null || echo 12) || exit 1
-make install || exit 1
+cmake -DCMAKE_INSTALL_PREFIX=../../`uname -s`/ -DBUILD_SHARED_LIBS=OFF -DBUILD_BULLET2_DEMOS=OFF -DBUILD_OPENGL3_DEMOS=OFF -DBUILD_CPU_DEMOS=OFF -DBUILD_PYBULLET=OFF -DUSE_DOUBLE_PRECISION=ON -DBUILD_EXTRAS=OFF -DBUILD_UNIT_TESTS=OFF -DCMAKE_BUILD_TYPE=RELEASE ../../src/bullet || exit 1
+cmake --build . --config release --target install
 echo "Completed build of Bullet."
 popd
 
