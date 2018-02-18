cb7f8f505ede50049719ff6ec32ebda2863e9b07
Chen Wenli <chenwenli@chenwenli.com>
2018-02-18 13:30:15 +0800

    try to fix build error in release version

--- Framework/Common/DebugManager.cpp
+++ Framework/Common/DebugManager.cpp
@@ -18,23 +18,28 @@ void DebugManager::Finalize()
 
 void DebugManager::Tick()
 {
+#ifdef DEBUG
     if(m_bDrawDebugInfo)
     {
         g_pGraphicsManager->ClearDebugBuffers();
         DrawDebugInfo();
         g_pPhysicsManager->DrawDebugInfo();
     }
+#endif
 }
 
 void DebugManager::ToggleDebugInfo()
 {
+#ifdef DEBUG
     m_bDrawDebugInfo = !m_bDrawDebugInfo;
     if(!m_bDrawDebugInfo)
         g_pGraphicsManager->ClearDebugBuffers();
+#endif
 }
 
 void DebugManager::DrawDebugInfo()
 {
+#ifdef DEBUG
     // x - axis
     Vector3f from (-1000.0f, 0.0f, 0.0f);
     Vector3f to (1000.0f, 0.0f, 0.0f);
@@ -52,4 +57,5 @@ void DebugManager::DrawDebugInfo()
     to.Set(0.0f, 0.0f, 1000.0f);
     color.Set(0.0f, 0.0f, 1.0f);
     g_pGraphicsManager->DrawLine(from, to, color);
+#endif
 }
