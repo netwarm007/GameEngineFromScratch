a93c095d2665f21802809b3c9e404cb1798286ac
Chen Wenli <chenwenli@chenwenli.com>
2018-03-15 10:31:18 +0800

    remove Game/article_45 from master

--- Game/article_45/article_45_config.cpp
+++
@@ -1,9 +0,0 @@
-#include "GfxConfiguration.h"
-#include "article_45_logic.hpp"
-#include "My/MyPhysicsManager.hpp"
-
-namespace My {
-    GfxConfiguration config(8, 8, 8, 8, 24, 8, 0, 960, 540, "article 45");
-    IGameLogic*       g_pGameLogic       = static_cast<IGameLogic*>(new article_45_logic);
-    IPhysicsManager*  g_pPhysicsManager  = static_cast<IPhysicsManager*>(new MyPhysicsManager);
-}
