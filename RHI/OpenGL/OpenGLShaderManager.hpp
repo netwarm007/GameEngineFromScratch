3d6ba590c82e361d8fa5b88642213ad6ed63896e
Chen Wenli <chenwenli@chenwenli.com>
2018-06-04 15:20:10 +0800

    [android] fix build error

--- RHI/OpenGL/OpenGLShaderManager.hpp
+++ RHI/OpenGL/OpenGLShaderManager.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "glad/glad.h"
 #include "OpenGLShaderManagerCommonBase.hpp"
 
 namespace My {
--- RHI/OpenGL/OpenGLShaderManagerCommonBase.hpp
+++ RHI/OpenGL/OpenGLShaderManagerCommonBase.hpp
@@ -1,5 +1,4 @@
 #pragma once
-#include "glad/glad.h"
 #include "IShaderManager.hpp"
 
 namespace My {
