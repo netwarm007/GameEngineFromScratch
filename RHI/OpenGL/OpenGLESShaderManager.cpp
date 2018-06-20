458e7bc179498db45de341db0a5370b8a767e501
Chen Wenli <chenwenli@chenwenli.com>
2018-06-20 21:29:00 +0800

    [android] fix build error

--- RHI/OpenGL/OpenGLESShaderManager.cpp
+++ RHI/OpenGL/OpenGLESShaderManager.cpp
@@ -2,6 +2,8 @@
 
 #define VS_SHADER_SOURCE_FILE "Shaders/OpenGLES/basic_vs.glsl"
 #define PS_SHADER_SOURCE_FILE "Shaders/OpenGLES/basic_ps.glsl"
+#define VS_SHADOWMAP_SOURCE_FILE "Shaders/shadowmap_vs.glsl"
+#define PS_SHADOWMAP_SOURCE_FILE "Shaders/shadowmap_ps.glsl"
 #define DEBUG_VS_SHADER_SOURCE_FILE "Shaders/OpenGLES/debug_vs.glsl"
 #define DEBUG_PS_SHADER_SOURCE_FILE "Shaders/OpenGLES/debug_ps.glsl"
 
