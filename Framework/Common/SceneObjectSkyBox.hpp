529b648083954e3b3e01c4b51a6cce7d813a4009
Chen Wenli <chenwenli@chenwenli.com>
2018-09-06 12:16:52 +0800

    import cmft

--- Framework/Common/SceneObjectSkyBox.hpp
+++ Framework/Common/SceneObjectSkyBox.hpp
@@ -14,29 +14,29 @@ namespace My {
             char filename[2048];
             const char fmt[] = "%s_%s.%s";
 
-            std::sprintf(filename, fmt, prefix, "rt", ext_name);
+            std::sprintf(filename, fmt, prefix, "posx", ext_name);
             m_Textures[0].SetName(filename);
 
-            std::sprintf(filename, fmt, prefix, "lf", ext_name);
+            std::sprintf(filename, fmt, prefix, "negx", ext_name);
             m_Textures[1].SetName(filename);
 
-            std::sprintf(filename, fmt, prefix, "bk", ext_name);
+            std::sprintf(filename, fmt, prefix, "posy", ext_name);
             m_Textures[2].SetName(filename);
 
-            std::sprintf(filename, fmt, prefix, "ft", ext_name);
+            std::sprintf(filename, fmt, prefix, "negy", ext_name);
             m_Textures[3].SetName(filename);
 
-            std::sprintf(filename, fmt, prefix, "up", ext_name);
+            std::sprintf(filename, fmt, prefix, "posz", ext_name);
             m_Textures[4].SetName(filename);
 
-            std::sprintf(filename, fmt, prefix, "dn", ext_name);
+            std::sprintf(filename, fmt, prefix, "negz", ext_name);
             m_Textures[5].SetName(filename);
         }
 
         inline SceneObjectTexture& GetRightTexture() { return m_Textures[0]; }
         inline SceneObjectTexture& GetLeftTexture()  { return m_Textures[1]; }
-        inline SceneObjectTexture& GetBackTexture()  { return m_Textures[2]; }
-        inline SceneObjectTexture& GetFrontTexture() { return m_Textures[3]; }
+        inline SceneObjectTexture& GetFrontTexture() { return m_Textures[2]; }
+        inline SceneObjectTexture& GetBackTexture()  { return m_Textures[3]; }
         inline SceneObjectTexture& GetTopTexture()   { return m_Textures[4]; }
         inline SceneObjectTexture& GetBottomTexture() { return m_Textures[5]; }
         inline SceneObjectTexture& GetTexture(uint32_t index) 
