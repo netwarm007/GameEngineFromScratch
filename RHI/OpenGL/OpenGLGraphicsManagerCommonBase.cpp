f5445d13de6052f36c3c116d39f3e96076fd5603
Chen Wenli <chenwenli@chenwenli.com>
2018-10-07 22:09:44 +0800

    gen mipmaps for material

--- RHI/OpenGL/OpenGLGraphicsManagerCommonBase.cpp
+++ RHI/OpenGL/OpenGLGraphicsManagerCommonBase.cpp
@@ -386,7 +386,8 @@ void OpenGLGraphicsManagerCommonBase::InitializeBuffers(const Scene& scene)
                         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
                         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
                         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-                        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+                        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+                        glGenerateMipmap(GL_TEXTURE_2D);
 
                         glBindTexture(GL_TEXTURE_2D, 0);
 
