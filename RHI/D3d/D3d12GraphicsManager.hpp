51e85e6c7407bf4e7cee4ef9faca3fc165169054
Chen Wenli <chenwenli@chenwenli.com>
2018-11-19 15:22:22 +0800

    refactor

--- RHI/D3d/D3d12GraphicsManager.hpp
+++ RHI/D3d/D3d12GraphicsManager.hpp
@@ -39,12 +39,15 @@ namespace My {
         HRESULT CreateRenderTarget();
         HRESULT CreateDepthStencil();
         HRESULT CreateGraphicsResources();
-        HRESULT CreateSamplerBuffer();
-        HRESULT CreateTextureBuffer();
-        HRESULT CreateTextureBuffer(SceneObjectTexture& texture);
-        HRESULT CreateConstantBuffer();
-        HRESULT CreateIndexBuffer(const SceneObjectIndexArray& index_array);
-        HRESULT CreateVertexBuffer(const SceneObjectVertexArray& v_property_array);
+
+        uint32_t CreateSamplerBuffer();
+        uint32_t CreateTextureBuffer(SceneObjectTexture& texture);
+        uint32_t CreateConstantBuffer();
+        size_t CreateIndexBuffer(const SceneObjectIndexArray& index_array);
+        size_t CreateVertexBuffer(const SceneObjectVertexArray& v_property_array);
+
+        void RegisterMsaaRtAsTexture();
+
         HRESULT CreateRootSignature();
         HRESULT WaitForPreviousFrame();
         HRESULT ResetCommandList();
@@ -52,20 +55,16 @@ namespace My {
         HRESULT CreateCommandList();
         HRESULT MsaaResolve();
 
-        HRESULT CreateInternalVertexBuffer();
-
     private:
         static const uint32_t           m_kFrameCount  = 2;
         static const uint32_t           m_kMaxTextureCount  = 2048;
-        static const uint32_t           m_kMaxObjectCount = 2048;
         static const uint32_t           m_kMaxLightCount = 100;
-		static const uint32_t		    m_kTextureDescStartIndex = m_kFrameCount * m_kMaxObjectCount * 2;
 
         ID3D12Device*                   m_pDev       = nullptr;             // the pointer to our Direct3D device interface
         D3D12_VIEWPORT                  m_ViewPort;                         // viewport structure
         D3D12_RECT                      m_ScissorRect;                      // scissor rect structure
         IDXGISwapChain3*                m_pSwapChain = nullptr;             // the pointer to the swap chain interface
-        ID3D12Resource*                 m_pRenderTargets[m_kFrameCount];      // the pointer to rendering buffer. [descriptor]
+        ID3D12Resource*                 m_pRenderTargets[m_kFrameCount];    // the pointer to rendering buffer. [descriptor]
         ID3D12Resource*                 m_pDepthStencilBuffer;              // the pointer to the depth stencil buffer
         ID3D12Resource*                 m_pMsaaRenderTarget;                // the pointer to the MSAA rendering target
         ID3D12CommandAllocator*         m_pCommandAllocator = nullptr;      // the pointer to command buffer allocator
@@ -75,6 +74,7 @@ namespace My {
         ID3D12DescriptorHeap*           m_pRtvHeap = nullptr;               // an array of descriptors of GPU objects
         ID3D12DescriptorHeap*           m_pDsvHeap = nullptr;               // an array of descriptors of GPU objects
 		ID3D12DescriptorHeap*           m_pCbvHeap = nullptr;               // an array of descriptors of GPU objects
+		ID3D12DescriptorHeap*           m_pSrvHeap = nullptr;               // an array of descriptors of GPU objects
         ID3D12DescriptorHeap*           m_pSamplerHeap = nullptr;           // an array of descriptors of GPU objects
         ID3D12PipelineState*            m_pPipelineState = nullptr;         // an object maintains the state of all currently set shaders
                                                                             // and certain fixed function state objects
@@ -86,16 +86,18 @@ namespace My {
         uint32_t                        m_nCbvSrvDescriptorSize;
         uint32_t                        m_nSamplerDescriptorSize;
 
-        std::vector<ID3D12Resource*>    m_Buffers;                          // the pointer to the vertex buffer
-        std::vector<ID3D12Resource*>    m_Textures;                          // the pointer to the vertex buffer
-        std::map<std::string, size_t>  m_TextureIndex;
-        std::vector<D3D12_VERTEX_BUFFER_VIEW>       m_VertexBufferView;                 // a view of the vertex buffer
-        std::vector<D3D12_INDEX_BUFFER_VIEW>        m_IndexBufferView;                  // a view of the vertex buffer
-        D3D12_VERTEX_BUFFER_VIEW                    m_VertexBufferViewResolve;
+        std::vector<ID3D12Resource*>    m_Buffers;                          // the pointer to the GPU buffer other than texture
+        std::vector<ID3D12Resource*>    m_Textures;                         // the pointer to the Texture buffer
+        std::map<std::string, uint32_t>   m_TextureIndex;                   // the LUT of texture name -> index
+        std::vector<D3D12_VERTEX_BUFFER_VIEW>       m_VertexBufferView;     // vertex buffer descriptors
+        std::vector<D3D12_INDEX_BUFFER_VIEW>        m_IndexBufferView;      // index buffer descriptors
+        std::vector<D3D12_CONSTANT_BUFFER_VIEW_DESC> m_ConstantBufferView;  // constant buffer descriptors
 
         struct D3dDrawBatchContext : public DrawBatchContext {
             uint32_t index_count;
+            size_t   index_offset;
             uint32_t property_count;
+            size_t   property_offset;
         };
 
         uint8_t*                        m_pCbvDataBegin = nullptr;
@@ -107,7 +109,5 @@ namespace My {
         HANDLE                          m_hFenceEvent;
         ID3D12Fence*                    m_pFence = nullptr;
         uint32_t                        m_nFenceValue;
-
-        size_t vertex_buffer_view_offset = 0;
     };
 }
