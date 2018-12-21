c7a0dc0dd1b1a7fc052d2088f8922a76d39ff730
Chen Wenli <chenwenli@chenwenli.com>
2018-12-21 13:53:08 +0800

    import libcef

---
+++ build_cef.bat
@@ -0,0 +1,9 @@
+@echo off
+'git submodule update --init External\src\cef
+mkdir External\build\cef
+pushd External\build\cef
+rm -rf *
+cmake -DCMAKE_INSTALL_PREFIX=../../ -DCMAKE_INSTALL_RPATH=../../ -DCMAKE_BUILD_TYPE=RELEASE ../../src/cef
+cmake --build . --config release --target install
+popd
+echo "Completed build of libcef_dll_wrapper"
