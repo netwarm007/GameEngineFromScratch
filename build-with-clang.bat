85243313373fb7513be753b643d1013d97f5e8b6
Chen Wenli <chenwenli@chenwenli.com>
2019-06-11 16:35:00 +0800

    update llvm and ispc

--- build-with-clang.bat
+++ build-with-clang.bat
@@ -1,6 +1,7 @@
-mkdir -p build
+mkdir build
 pushd build
 rm -rf *
+set PATH=%PATH%;..\External\Windows\bin
 cmake -G "NMake Makefiles" -DCMAKE_TOOLCHAIN_FILE=..\cmake\clang-cl.cmake ..
 cmake --build . --config debug
 popd
