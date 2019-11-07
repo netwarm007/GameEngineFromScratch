c2760084c8f1dd853f489a681ab3280647a8ece2
SijieShen <sijie.shen@dji.com>
2019-11-07 19:28:45 +0900

    fix cpp Transform bug

--- Framework/GeomMath/cpp/Transform.cpp
+++ Framework/GeomMath/cpp/Transform.cpp
@@ -2,12 +2,16 @@ namespace Dummy
 {
     void Transform(float vector[4], const float matrix[16])
     {
+        float result[4];
         for(int index = 0; index < 4; index++)
         {
-            vector[index] = (vector[0] * matrix[index]) 
+            result[index] = (vector[0] * matrix[index]) 
                 + (vector[1] * matrix[index + 4]) 
                 + (vector[2] * matrix[index + 8]) 
                 + (vector[3] * matrix[index + 12]);
+        }
+        for (int index = 0; index < 4; index++) {
+            vector[index] = result[index];
         }
     }
 }
