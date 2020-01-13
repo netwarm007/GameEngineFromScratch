3cc4f7d4ae38c17758f699b3f0cae37eeb418613
Chen Wenli <chenwenli@chenwenli.com>
2020-01-13 14:26:24 +0800

    update ispc and llvm

---
+++ build_ispc.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+git submodule update --init External/src/ispc
+mkdir -p External/build/ispc
+cd External/build/ispc
+export INSTALL_ROOT=../../`uname -s`
+export LLVM_HOME=../../src/llvm
+export LLVM_VERSION=LLVM_8_0
+export ISPC_HOME=../../src/ispc
+export PATH=$INSTALL_ROOT/bin:$PATH
+cmake -DCMAKE_INSTALL_PREFIX=$INSTALL_ROOT -DARM_ENABLED=ON -DNVPTX_ENABLED=OFF -DISPC_INCLUDE_EXAMPLES=OFF $ISPC_HOME
+make -j4
+make install
+
