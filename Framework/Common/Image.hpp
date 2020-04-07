e9d4eedff975891ddd80cf8a35e264593206c061
Chen Wenli <chenwenli@chenwenli.com>
2020-04-07 10:03:55 +0800

    fix linux build

--- Framework/Common/Image.hpp
+++ Framework/Common/Image.hpp
@@ -7,22 +7,22 @@
 namespace My {
 
     struct Image {
-        uint32_t Width;
-        uint32_t Height;
-        uint8_t* data;
-        uint32_t bitcount;
-        uint32_t pitch;
-        size_t  data_size;
-        bool    compressed;
-        bool    is_float;
-        uint32_t compress_format;
+        uint32_t Width{0};
+        uint32_t Height{0};
+        uint8_t* data{nullptr};
+        uint32_t bitcount{0};
+        uint32_t pitch{0};
+        size_t  data_size{0};
+        bool    compressed{false};
+        bool    is_float{false};
+        uint32_t compress_format{0};
         uint32_t mipmap_count{1};
         struct Mipmap {
-            uint32_t Width;
-            uint32_t Height;
-            uint32_t pitch;
-            size_t offset;
-            size_t data_size;
+            uint32_t Width{0};
+            uint32_t Height{0};
+            uint32_t pitch{0};
+            size_t offset{0};
+            size_t data_size{0};
         } mipmaps[10];
 
         Image() 
