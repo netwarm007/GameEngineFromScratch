051b71dd45387eeb05dbcfb85120d00e4eb88f35
Chen Wenli <chenwenli@chenwenli.com>
2020-04-27 15:28:36 +0800

    bump llvm to 10.0

--- build_llvm_clang_libcxx.bat
+++
@@ -1,9 +0,0 @@
-git submodule update --init External/src/llvm External/src/clang External/src/libcxx External/src/libcxxabi
-mkdir External\build\llvm
-pushd External\build\llvm
-cmake -DLLVM_ENABLE_DUMP=ON -DCMAKE_INSTALL_PREFIX=../../Windows  -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD="AMDGPU;ARM;X86;AArch64;NVPTX" -DLLVM_ENABLE_PROJECTS="clang;libcxx;libcxxabi" -G "Visual Studio 16 2019" -Thost=x64 ../../src/llvm
-cmake --build . --config Release --target install
-make install-cxx
-make install-cxx-abi
-popd
-
