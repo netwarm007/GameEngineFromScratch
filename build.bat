bd5f61f43db3febea6ad189fe8c29c095190e847
Chen Wenli <chenwenli@chenwenli.com>
2020-04-30 10:02:55 +0800

    try to use ninja on appveyor

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 mkdir build
 pushd build
-cmake -DCMAKE_BUILD_TYPE=Debug -G "Visual Studio 16 2019" -A "x64" -Thost=x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cmake -G "Visual Studio 16 2019" -A "x64" -Thost=x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cmake --build . --config Debug
 popd
 
