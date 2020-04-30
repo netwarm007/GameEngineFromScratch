b17925ed737ed6e1d25a1b70fbf5c40b6e658b72
Chen Wenli <chenwenli@chenwenli.com>
2020-04-30 09:53:16 +0800

    fix appvoyer build error

--- build_glslangValidator.bat
+++ build_glslangValidator.bat
@@ -3,7 +3,7 @@ git submodule update --init External\src\glslang
 mkdir External\build\glslang
 pushd External\build\glslang
 rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=../../Windows/ -DCMAKE_INSTALL_RPATH=../../Windows/ -DCMAKE_BUILD_TYPE=RELEASE ../../src/glslang
+cmake -DCMAKE_INSTALL_PREFIX=../../Windows/ -DCMAKE_INSTALL_RPATH=../../Windows/ -DBUILD_EXTERNAL=NO ../../src/glslang
 cmake --build . --config Release --target install
 popd
 echo "Completed build of glslangValidator"
--- build_glslangValidator.sh
+++ build_glslangValidator.sh
@@ -4,6 +4,6 @@ git submodule update --init External/src/glslang
 mkdir -p External/build/glslang
 pushd External/build/glslang
 rm -rf *
-cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../../`uname -s`/ -DCMAKE_INSTALL_RPATH=../../`uname -s`/ ../../src/glslang
+cmake -G "Ninja" -DCMAKE_INSTALL_PREFIX=../../`uname -s`/ -DCMAKE_INSTALL_RPATH=../../`uname -s`/ -DBUILD_EXTERNAL=NO ../../src/glslang
 cmake --build . --config Release --target install
 popd
