9b9069ae14f4f2a20d9e08a7baba8e882fe0d58b
Chen Wenli <chenwenli@chenwenli.com>
2020-05-22 08:38:58 +0800

    rust hello-engine

---
+++ Binding/rust/hello-engine/src/main.rs
@@ -0,0 +1,24 @@
+use winit::{
+    event::{Event, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+fn main() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+            .with_title("Hello Engine!")
+            .build(&event_loop).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        *control_flow = ControlFlow::Wait;
+
+        match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => (),
+        }
+    });
+}
