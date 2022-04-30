76c92c8b2da76bbc9aff51eb3e018b68ba58f669
netwarm007 <chenwenli@chenwenli.com>
2022-04-30 23:33:22 +0800

    upgrade to vs2022

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 mkdir build
 pushd build
-cmake -G "Visual Studio 16 2019" -A "x64" -Thost=x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DOPENGL_RHI_DEBUG=ON -DD3D12_RHI_DEBUG=ON ..
+cmake -G "Visual Studio 17 2022" -A "x64" -Thost=x64 -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DOPENGL_RHI_DEBUG=ON -DD3D12_RHI_DEBUG=ON ..
 cmake --build . --config Debug
 popd
 
--- scripts/build_cef.bat
+++ scripts/build_cef.bat
@@ -3,7 +3,7 @@ git submodule update --init External\src\cef
 mkdir External\build\cef
 pushd External\build\cef
 rm -rf *
-cmake -DCMAKE_INSTALL_PREFIX=../../ -DCMAKE_INSTALL_RPATH=../../ -DCMAKE_BUILD_TYPE=DEBUG -DCEF_RUNTIME_LIBRARY_FLAG="/MD" -G "Visual Studio 16 2019" -A "x64" ../../src/cef
+cmake -DCMAKE_INSTALL_PREFIX=../../ -DCMAKE_INSTALL_RPATH=../../ -DCMAKE_BUILD_TYPE=DEBUG -DCEF_RUNTIME_LIBRARY_FLAG="/MD" -G "Visual Studio 17 2022" -A "x64" ../../src/cef
 cmake --build . --config Debug --target install
 popd
 echo "Completed build of libcef_dll_wrapper"
