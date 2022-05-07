35ab567cacbeb520a7f8e89985551b3061721fc3
Chen Wenli <chenwenli@chenwenli.com>
2022-05-07 15:48:52 +0800

    bump submodule version

--- scripts/build_bison.sh
+++ scripts/build_bison.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 git submodule update --init External/src/bison
+pushd External/src/bison
+./bootstrap
+popd
 mkdir -p External/build/bison
 cd External/build/bison
 ../../src/bison/configure --prefix=$(pwd)/../../`uname -s`/
