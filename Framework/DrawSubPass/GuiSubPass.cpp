c9e181f7e9fb14a0f800dfc14dcb29dde827d028
Chen Wenli <chenwenli@chenwenli.com>
2022-05-18 20:52:56 +0800

    change default GUI

--- Framework/DrawSubPass/GuiSubPass.cpp
+++ Framework/DrawSubPass/GuiSubPass.cpp
@@ -7,8 +7,13 @@ void GuiSubPass::Draw(Frame& frame) {
     if (ImGui::GetCurrentContext()) {
         ImGui::NewFrame();
 
-        ImGui::ShowDemoWindow();
+        ImGui::Begin("Debug Window", nullptr, ImGuiWindowFlags_AlwaysAutoResize
+                                                        | ImGuiWindowFlags_NoFocusOnAppearing);
+        ImGui::Text("Hello! I'm a Game Engine From Scratch!");
+        ImGui::End();
 
         ImGui::Render();
+
+        ImGui::EndFrame();
     }
 }
