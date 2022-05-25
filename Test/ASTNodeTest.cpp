6219a2c1e41282987b9916a3b62469a1e68b420d
Chen Wenli <chenwenli@chenwenli.com>
2022-05-25 13:20:10 +0800

    fix compile issue with non-gcc

--- Test/ASTNodeTest.cpp
+++ Test/ASTNodeTest.cpp
@@ -14,13 +14,13 @@ int main(int argc, char* argv[])
     auto ns     = std::make_shared<ASTNodeNameSpace<const char*>>( "namespace", "My" );
 
     ASTList<std::string> enum_items { "A", "B", "C", "D", "E" };
-    auto enum_  = std::make_shared<ASTNodeEnum<typeof(enum_items)>> ( "Enum", std::move(enum_items) );
+    auto enum_  = std::make_shared<ASTNodeEnum<decltype(enum_items)>> ( "Enum", std::move(enum_items) );
 
     ASTList<ASTPair<std::string, std::string>> field_list { {"field1","int"}, {"field2","double"}, {"field3","string"} };
-    auto struct_= std::make_shared<ASTNodeStruct<typeof(field_list)>>( "Struct", std::move(field_list) );
+    auto struct_= std::make_shared<ASTNodeStruct<decltype(field_list)>>( "Struct", std::move(field_list) );
 
     ASTList<ASTPair<std::string, std::string>> record_list { {"field1","uuid"}, {"field2","Texture"}, {"field3","Shader"} };
-    auto table  = std::make_shared<ASTNodeTable<typeof(record_list)>>( "Table", std::move(record_list) );
+    auto table  = std::make_shared<ASTNodeTable<decltype(record_list)>>( "Table", std::move(record_list) );
 
     root->SetLeft(ns);
     ns->SetLeft(enum_);
