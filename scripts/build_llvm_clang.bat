cd5a6ea4c6f0d846be613408d0494c7ce13d1f4a
netwarm007 <chenwenli@chenwenli.com>
2022-06-16 09:23:00 +0800

    fix windows build scripts

--- scripts/build_llvm_clang.bat
+++ scripts/build_llvm_clang.bat
@@ -1,7 +1,7 @@
 git submodule update --init External/src/llvm
 mkdir External\build\llvm
 pushd External\build\llvm
-cmake -DLLVM_ENABLE_DUMP=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_INSTALL_UTILS=ON -DCMAKE_INSTALL_PREFIX=../../Windows  -DLLVM_TARGETS_TO_BUILD="AMDGPU;ARM;X86;AArch64;NVPTX" -DLLVM_ENABLE_PROJECTS="clang" -G "Visual Studio 16 2022" -A "x64" -Thost=x64 ../../src/llvm/llvm
+cmake -DLLVM_ENABLE_DUMP=ON -DLLVM_ENABLE_ASSERTIONS=ON -DLLVM_INSTALL_UTILS=ON -DCMAKE_INSTALL_PREFIX=../../Windows  -DLLVM_TARGETS_TO_BUILD="AMDGPU;ARM;X86;AArch64;NVPTX" -DLLVM_ENABLE_PROJECTS="clang" -G "Visual Studio 17 2022" -A "x64" -Thost=x64 ../../src/llvm/llvm
 if "%1" == "" (cmake --build . --config Debug --target install) else (cmake --build . --config %1 --target install)
 popd
 
