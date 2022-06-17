0bd27c7695f52d4ff9be8397693c53b0469d2b70
netwarm007 <chenwenli@chenwenli.com>
2022-06-18 00:28:41 +0800

    search asset from exe path (windows)

--- Platform/Windows/AssetLoader.cpp
+++ Platform/Windows/AssetLoader.cpp
@@ -1,6 +1,16 @@
 
 #include "AssetLoader.hpp"
+#include <Windows.h>
+#include <string_view>
 
 namespace My {
-int AssetLoader::Initialize() { return 0; }
+int AssetLoader::Initialize() {
+    char buffer[MAX_PATH];
+    GetModuleFileName(NULL, buffer, sizeof(buffer));
+    std::string::size_type pos = std::string_view(buffer).find_last_of("\\/");
+
+    m_strTargetPath = std::string_view(buffer).substr(0, pos);
+
+    return 0;
+}
 }  // namespace My
