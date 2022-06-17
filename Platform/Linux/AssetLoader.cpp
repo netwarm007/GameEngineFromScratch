4e1a7e36b660b98610feef35cfd64a5048142a2f
netwarm007 <chenwenli@chenwenli.com>
2022-06-18 00:08:13 +0800

    fix build error

---
+++ Platform/Linux/AssetLoader.cpp
@@ -0,0 +1,5 @@
+#include "AssetLoader.hpp"
+
+namespace My {
+int AssetLoader::Initialize() { return 0; }
+}  // namespace My
---
+++ Platform/Windows/AssetLoader.cpp
@@ -0,0 +1,6 @@
+
+#include "AssetLoader.hpp"
+
+namespace My {
+int AssetLoader::Initialize() { return 0; }
+}  // namespace My
