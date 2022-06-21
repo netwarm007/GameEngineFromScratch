bfd334fca2b5227fb4ea3bdac56e27c70af5d15e
netwarm007 <chenwenli@chenwenli.com>
2022-06-21 13:44:25 +0800

    bug fix

--- Framework/SceneGraph/SceneCameraNode.hpp
+++ Framework/SceneGraph/SceneCameraNode.hpp
@@ -4,14 +4,12 @@
 namespace My {
 class SceneCameraNode : public SceneNode<SceneObjectCamera> {
    protected:
-    Vector3f m_Target = {0.0f};
+    Vector3f m_Target{0.0f, 0.0f, 0.0f};
 
    public:
     using SceneNode::SceneNode;
 
-    void SetTarget(const Vector3f& target) { 
-        m_Target = target; 
-    };
+    void SetTarget(const Vector3f& target) { m_Target = target; };
     const Vector3f& GetTarget() { return m_Target; };
     Matrix3X3f GetLocalAxis() override {
         Matrix3X3f result;
@@ -19,7 +17,7 @@ class SceneCameraNode : public SceneNode<SceneObjectCamera> {
         Vector3f target = GetTarget();
         auto camera_position = Vector3f(0.0f);
         TransformCoord(camera_position, *pTransform);
-        Vector3f camera_z_axis ({0.0f, 0.0f, 1.0f});
+        Vector3f camera_z_axis({0.0f, 0.0f, 1.0f});
         Vector3f camera_y_axis = target - camera_position;
         Normalize(camera_y_axis);
         Vector3f camera_x_axis;
