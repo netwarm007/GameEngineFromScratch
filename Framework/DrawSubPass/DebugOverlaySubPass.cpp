1b5515c864265e3fcb6dce2c2e11b5810cde23f8
netwarm007 <chenwenli@chenwenli.com>
2022-07-04 21:59:02 +0800

    tiny adjustment

--- Framework/DrawSubPass/DebugOverlaySubPass.cpp
+++ Framework/DrawSubPass/DebugOverlaySubPass.cpp
@@ -12,8 +12,8 @@ void DebugOverlaySubPass::Draw(Frame& frame) {
         ImGui::Begin((const char*)u8"调试窗口");  // Create a debug window
 
         ImGui::Text(
-            (const char*)u8"阴影贴图");  // Display some text (you can use
-                                            // a format strings too)
+            (const char*)u8"阴影贴图");
+
         for (int32_t i = 0; i < frame.frameContext.globalShadowMap.size; i++) {
             ImGui::Image((ImTextureID)frame.frameContext.globalShadowMap.handler, ImVec2(128, 128));
         }
@@ -26,13 +26,16 @@ void DebugOverlaySubPass::Draw(Frame& frame) {
             ImGui::Image((ImTextureID)frame.frameContext.cubeShadowMap.handler, ImVec2(128, 128));
         }
 
-        ImGui::Text(
-            (const char*)u8"天空盒");  // Display some text (you can use
-                                            // a format strings too)
         // Draw Skybox
+        ImGui::Text(
+            (const char*)u8"天空盒");
+
         ImGui::Image((ImTextureID)frame.skybox.handler, ImVec2(128, 128));
 
         // BRDF LUT
+        ImGui::Text(
+            (const char*)u8"BRDF LUT");
+
         ImGui::Image((ImTextureID)frame.brdfLUT.handler, ImVec2(128, 128));
 
         ImGui::End(); // finish the debug window
