ad2832466f6d065fccec7ac04ac58e9682c95dfb
Chen Wenli <chenwenli@chenwenli.com>
2022-11-19 22:28:14 +0800

    try to fix android build

--- scripts/build_openal-android.sh
+++ scripts/build_openal-android.sh
@@ -3,7 +3,7 @@ set -e
 git submodule update --init External/src/openal
 mkdir -p External/build/openal
 pushd External/build/openal
-cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-24 -DANDROID_STL=c++_static -DCMAKE_INSTALL_PREFIX=../../Android -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DLIBTYPE=STATIC ../../src/openal
+cmake -G "Ninja" -DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake -DANDROID_ABI=arm64-v8a -DANDROID_PLATFORM=android-24 -DCMAKE_INSTALL_PREFIX=../../Android/ -DCMAKE_INSTALL_RPATH=../../Android/ ../../src/openal
 if [[ -z $1 ]];
 then
     cmake --build . --config Debug --target install
