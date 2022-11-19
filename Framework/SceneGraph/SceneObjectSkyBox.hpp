37d00d068e4c09392fd7a640a53cf2e83002801a
Chen Wenli <chenwenli@chenwenli.com>
2022-11-19 20:33:36 +0800

    eliminate compile warning

--- Framework/SceneGraph/SceneObjectSkyBox.hpp
+++ Framework/SceneGraph/SceneObjectSkyBox.hpp
@@ -14,68 +14,69 @@ class SceneObjectSkyBox : public BaseSceneObject {
     SceneObjectSkyBox& operator=(SceneObjectSkyBox&& rhs) = delete;
 
     void SetName(const char* prefix, const char* ext_name) {
-        char filename[2048];
+        const size_t FILENAME_SIZE = 2048;
+        char filename[FILENAME_SIZE];
         const char fmt[] = "%s_%s.%s";
         uint32_t index = 0;
 
         //////////////////
         // Sky Box
-        std::sprintf(filename, fmt, prefix, "posx", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "posx", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "negx", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "negx", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "posy", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "posy", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "negy", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "negy", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "posz", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "posz", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "negz", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "negz", ext_name);
         m_Textures[index++].SetName(filename);
 
         //////////////////
         // Irradiance Map
-        std::sprintf(filename, fmt, prefix, "irradiance_posx", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "irradiance_posx", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "irradiance_negx", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "irradiance_negx", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "irradiance_posy", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "irradiance_posy", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "irradiance_negy", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "irradiance_negy", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "irradiance_posz", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "irradiance_posz", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "irradiance_negz", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "irradiance_negz", ext_name);
         m_Textures[index++].SetName(filename);
 
         //////////////////
         // Radiance Map
-        std::sprintf(filename, fmt, prefix, "radiance_posx", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "radiance_posx", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "radiance_negx", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "radiance_negx", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "radiance_posy", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "radiance_posy", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "radiance_negy", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "radiance_negy", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "radiance_posz", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "radiance_posz", ext_name);
         m_Textures[index++].SetName(filename);
 
-        std::sprintf(filename, fmt, prefix, "radiance_negz", ext_name);
+        std::snprintf(filename, FILENAME_SIZE, fmt, prefix, "radiance_negz", ext_name);
         m_Textures[index++].SetName(filename);
     }
 
