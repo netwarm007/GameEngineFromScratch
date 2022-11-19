913af38a8536bf537a2f56ae698c4915e59b1302
Chen Wenli <chenwenli@chenwenli.com>
2022-11-19 22:52:59 +0800

    try to fix windows build

--- scripts/build_openal.bat
+++ scripts/build_openal.bat
@@ -2,6 +2,6 @@
 git submodule update --init External\src\openal
 mkdir External\build\openal
 pushd External\build\openal
-cmake -DCMAKE_INSTALL_PREFIX=..\..\Windows\ -G "Visual Studio 16 2019" -A "x64" -Thost=x64 -DALSOFT_UTILS=OFF -DALSOFT_EXAMPLES=OFF -DLIBTYPE=STATIC ..\..\src\openal
+cmake -DCMAKE_INSTALL_PREFIX=..\..\Windows\ -G "Visual Studio 17 2022" -A "x64" -Thost=x64 ..\..\src\openal
 if "%1" == "" (cmake --build . --config Debug --target install) else (cmake --build . --config %1 --target install)
 popd
