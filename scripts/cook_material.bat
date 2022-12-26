c18042d30827a706ec0be20b3731be7575cb387c
netwarm007 <chenwenli@chenwenli.com>
2022-12-27 00:14:36 +0800

    enable cmake CUDA & ISPC native support

--- scripts/cook_material.bat
+++ scripts/cook_material.bat
@@ -1,4 +1,4 @@
 mkdir build\Asset\Materials
 pushd build\Asset\Materials
-if "%1" == "" (..\..\Utility\Debug\MaterialBaker.exe) else (..\..\Utility\%1\MaterialBaker.exe)
+if "%1" == "" (..\..\Utility\MaterialBaker.exe) else (..\..\Utility\MaterialBaker.exe)
 popd
