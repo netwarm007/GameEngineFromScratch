4575dac97d0d89f592e0f0604f42ec346f797e92
netwarm007 <chenwenli@chenwenli.com>
2022-12-27 09:37:50 +0800

    fix appvayor build

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 mkdir build
 pushd build
-cmake -G "Ninja" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DOPENGL_RHI_DEBUG=ON -DD3D12_RHI_DEBUG=ON ..
+cmake -G "NMake Makefiles" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DOPENGL_RHI_DEBUG=ON -DD3D12_RHI_DEBUG=ON ..
 if "%1" == "" (cmake --build . --config Debug) else (cmake --build . --config %1)
 popd
 
