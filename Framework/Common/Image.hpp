netwarm007/GameEngineFromScratch

commit 1b56d7287e92e13d534553460993307b34906920

basic cuda integration

Framework/Common/Image.hpp
@@ -5,8 +5,13 @@
 
 #include "config.h"
 #include "geommath.hpp"
+#include "half_float.hpp"
 
 namespace My {
+inline int to_unorm(Scalar auto f) {
+    return My::clamp(f, decltype(f)(0.0), decltype(f)(0.999)) * 256;
+}
+
 enum class COMPRESSED_FORMAT : uint16_t {
     NONE,
     DXT1,
@@ -114,7 +119,7 @@ struct Image {
         if (data) delete[] data;
     }
 
-    int GetR(uint32_t x, uint32_t y) const {
+    float GetR(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0;
 
         switch (pixel_format) {
@@ -124,22 +129,21 @@ struct Image {
             case PIXEL_FORMAT::RG8:
             case PIXEL_FORMAT::RGB8:
             case PIXEL_FORMAT::RGBA8:
-                return *(data + y * pitch + x * (bitcount >> 3));
+                return *(data + y * pitch + x * (bitcount >> 3)) / 256.0;
             case PIXEL_FORMAT::R5G6B5:
-                return (*(data + y * pitch + x * (bitcount >> 3)) & 0xF8) >> 3;
+                return ((*(data + y * pitch + x * (bitcount >> 3)) & 0xF8) >> 3) / 32.0;
             case PIXEL_FORMAT::R16:
             case PIXEL_FORMAT::RG16:
             case PIXEL_FORMAT::RGB16:
             case PIXEL_FORMAT::RGBA16:
-                return *(data + y * pitch + x * (bitcount >> 3) + 1);
+                return float32(*(uint16_t *)(data + y * pitch + x * (bitcount >> 3)));
             case PIXEL_FORMAT::R32:
             case PIXEL_FORMAT::RG32:
             case PIXEL_FORMAT::RGB32:
             case PIXEL_FORMAT::RGBA32:
-                return *(data + y * pitch + x * (bitcount >> 3) + 3);
+                return *(float *)(data + y * pitch + x * (bitcount >> 3));
             case PIXEL_FORMAT::R10G10B10A2:
-                // not supported
-                return 0;
+                return (*(uint32_t *)(data + y * pitch + x * (bitcount >> 3)) >> 22) / 1024.0;
             default:
                 assert(0);
         }
@@ -147,7 +151,7 @@ struct Image {
         return 0;
     }
 
-    int GetG(uint32_t x, uint32_t y) const {
+    float GetG(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0;
 
         switch (pixel_format) {
@@ -155,26 +159,27 @@ struct Image {
             case PIXEL_FORMAT::R8:
             case PIXEL_FORMAT::R16:
             case PIXEL_FORMAT::R32:
-                return GetR(x, y);
+                return 0;
             case PIXEL_FORMAT::RG8:
             case PIXEL_FORMAT::RGB8:
             case PIXEL_FORMAT::RGBA8:
-                return *(data + y * pitch + x * (bitcount >> 3) + 1);
+                return *(data + y * pitch + x * (bitcount >> 3) + 1) / 256.0;
             case PIXEL_FORMAT::RG16:
             case PIXEL_FORMAT::RGB16:
             case PIXEL_FORMAT::RGBA16:
+                return float32(*(uint16_t *)(data + y * pitch + x * (bitcount >> 3) + 2));
             case PIXEL_FORMAT::RG32:
             case PIXEL_FORMAT::RGB32:
             case PIXEL_FORMAT::RGBA32:
+                return *(float *)(data + y * pitch + x * (bitcount >> 3) + 4);
             case PIXEL_FORMAT::R10G10B10A2:
-                // not supported
-                return 0;
+                return ((*(uint32_t *)(data + y * pitch + x * (bitcount >> 3)) >> 12) & 0x3FF) / 1024.0;
             case PIXEL_FORMAT::R5G6B5:
-                return ((*(data + y * pitch + x * (bitcount >> 3)) & 0x07)
+                return (((*(data + y * pitch + x * (bitcount >> 3)) & 0x07)
                         << 3) +
                        ((*(data + y * pitch + x * (bitcount >> 3) + 1) &
                          0xE0) >>
-                        5);
+                        5)) / 64.0;
             default:
                 assert(0);
         }
@@ -182,7 +187,7 @@ struct Image {
         return 0;
     }
 
-    int GetB(uint32_t x, uint32_t y) const {
+    float GetB(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0;
 
         switch (pixel_format) {
@@ -190,22 +195,24 @@ struct Image {
             case PIXEL_FORMAT::R8:
             case PIXEL_FORMAT::R16:
             case PIXEL_FORMAT::R32:
-                return GetR(x, y);
+                return 0;
             case PIXEL_FORMAT::RG8:
-                return GetG(x, y);
+                return 0;
             case PIXEL_FORMAT::RGB8:
             case PIXEL_FORMAT::RGBA8:
-                return *(data + y * pitch + x * (bitcount >> 3) + 2);
-                break;
+                return *(data + y * pitch + x * (bitcount >> 3) + 2) / 256.0;
             case PIXEL_FORMAT::RG16:
+                return 0;
             case PIXEL_FORMAT::RGB16:
             case PIXEL_FORMAT::RGBA16:
+                return float32(*(uint16_t *)(data + y * pitch + x * (bitcount >> 3) + 4));
             case PIXEL_FORMAT::RG32:
+                return 0;
             case PIXEL_FORMAT::RGB32:
             case PIXEL_FORMAT::RGBA32:
+                return *(float *)(data + y * pitch + x * (bitcount >> 3) + 8);
             case PIXEL_FORMAT::R10G10B10A2:
-                // not supported
-                return 0;
+                return ((*(uint32_t *)(data + y * pitch + x * (bitcount >> 3)) >> 2) & 0x3FF) / 1024.0;
             case PIXEL_FORMAT::R5G6B5:
                 return (*(data + y * pitch + x * (bitcount >> 3) + 1) & 0x1F);
             default:
@@ -215,7 +222,7 @@ struct Image {
         return 0;
     }
 
-    int GetA(uint32_t x, uint32_t y) const {
+    float GetA(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0xFF;
 
         switch (pixel_format) {
@@ -226,18 +233,21 @@ struct Image {
             case PIXEL_FORMAT::RG8:
             case PIXEL_FORMAT::RGB8:
             case PIXEL_FORMAT::R5G6B5:
-                return 0xFF;
+                return 1.0;
             case PIXEL_FORMAT::RGBA8:
-                return *(data + y * pitch + x * (bitcount >> 3) + 3);
+                return *(data + y * pitch + x * (bitcount >> 3) + 3) / 256.0;
             case PIXEL_FORMAT::RG16:
             case PIXEL_FORMAT::RGB16:
+                return 1.0;
             case PIXEL_FORMAT::RGBA16:
+                return float32(*(uint16_t *)(data + y * pitch + x * (bitcount >> 3) + 6));
             case PIXEL_FORMAT::RG32:
             case PIXEL_FORMAT::RGB32:
+                return 1.0;
             case PIXEL_FORMAT::RGBA32:
+                return *(float *)(data + y * pitch + x * (bitcount >> 3) + 12);
             case PIXEL_FORMAT::R10G10B10A2:
-                // not supported
-                return 0xFF;
+                return (*(uint32_t *)(data + y * pitch + x * (bitcount >> 3)) & 0x3) / 4.0;
             default:
                 assert(0);
         }
@@ -424,7 +434,6 @@ struct Image {
 
     void SaveTGA(const char* filename) const {
         assert(filename != NULL);
-        assert((pixel_format == PIXEL_FORMAT::RGBA8) || (pixel_format == PIXEL_FORMAT::RGB8));
 
         if (compressed) {
             fprintf(stderr, "SaveTGA is called but the image is compressed.\n");
@@ -446,16 +455,10 @@ struct Image {
             else if (i == 15)
                 fprintf(file, "%c", (Height & 0xFF00) >> 8);
             else if (i == 16) {
-                fprintf(file, "%c", bitcount);
+                fprintf(file, "%c", 32);
             }
             else if (i == 17) {
-                switch (bitcount) {
-                    case 24: fprintf(file, "%c", 0x00);
-                             break;
-                    case 32: fprintf(file, "%c", 0x08);
-                             break;
-                    default: assert(0);
-                } 
+                fprintf(file, "%c", 0x08);
             }
             else
                 fprintf(file, "%c", 0);
@@ -464,11 +467,10 @@ struct Image {
         for (uint32_t y = 0; y < Height; y++) {
             for (uint32_t x = 0; x < Width; x++) {
                 // note reversed order: b, g, r
-                fputc(GetB(x, y) & 0xFF, file);
-                fputc(GetG(x, y) & 0xFF, file);
-                fputc(GetR(x, y) & 0xFF, file);
-                if (pixel_format == PIXEL_FORMAT::RGBA8)
-                    fputc(GetA(x, y) & 0xFF, file);
+                fputc(to_unorm(GetB(x, y)), file);
+                fputc(to_unorm(GetG(x, y)), file);
+                fputc(to_unorm(GetR(x, y)), file);
+                fputc(to_unorm(GetA(x, y)), file);
             }
         }
         fclose(file);

.circleci/config.yml
@@ -410,7 +410,7 @@ workflows:
   version: 2
   build-and-test: #for branch
     jobs:
-      #- build_windows
+      - build_windows
       - build_linux
       - build_macos
       #- build_android

CMakeLists.txt
@@ -4,6 +4,7 @@ source_group(TREE ${PROJECT_SOURCE_DIR})
 
 set(CMAKE_C_STANDARD 11)
 set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 set(THREADS_PREFER_PTHREAD_FLAG ON)
@@ -13,7 +14,17 @@ include(CheckLanguage)
 check_language(CUDA)
 if(CMAKE_CUDA_COMPILER)
     enable_language(CUDA)
-    set(USE_CUDA ON)
+    if (NOT CMAKE_CUDA_COMPILER_VERSION VERSION_LESS 12.0)
+        if(CMAKE_CUDA_SIMULATE_VERSION VERSION_GREATER_EQUAL 19.11.25505)
+            set(CMAKE_CUDA20_STANDARD_COMPILE_OPTION "-std=c++20")
+            set(CMAKE_CUDA20_EXTENSION_COMPILE_OPTION "-std=c++20")
+        endif()
+        set(CMAKE_CUDA_STANDARD 20)
+        set(CMAKE_CUDA_STANDARD_REQUIRED ON)
+        set(USE_CUDA ON)
+    else()
+        message(STATUS "Need CUDA Toolkit 12.0+")
+    endif()
 else()
     message(STATUS "No CUDA support")
 endif()

Framework/GeomMath/geommath.hpp
@@ -255,14 +255,6 @@ Vector<T, N> operator-(const Vector<T, N>& vec1, const Vector<T, N>& vec2) {
     return result;
 }
 
-template <class T, Dimension auto N>
-Vector<T, N> operator-(const Vector<T, N>& vec, const Scalar auto scalar) {
-    Vector<T, N> result(scalar);
-    VectorSub(result, vec, result);
-
-    return result;
-}
-
 template <class T>
 inline void CrossProduct(T& result, const Vector<T, 2>& vec1,
                          const Vector<T, 2>& vec2) {

Framework/GeomMath/half_float.hpp
@@ -1,0 +1,31 @@
+#pragma once
+// based on https://gist.github.com/mertin-kallman/5049614
+// float32
+// Martin Kallman
+//
+// Fast half-precision to single-precision floating point conversion
+//  - Supports signed zero and denormals-as-zero (DAZ)
+//  - Does not support infinities or NaN
+//  - Few, partically pipelinable, non-branching instructions,
+//  - Core operations ~6 clock cycles on modern x86-64
+
+inline float float32(const uint16_t in) {
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t t3;
+
+    t1 = in & 0x7fffu;      // Non-sign bits
+    t2 = in & 0x8000u;      // Sign bit
+    t3 = in & 0x7c00u;      // Exponent
+
+    t1 <<= 13u;             // Align mantissa on MSB
+    t2 <<= 16u;             // Shift sign bit into position
+
+    t1 += 0x38000000;       // Adjust bias
+
+    t1 = (t3 == 0 ? 0 : t1);
+
+    t1 |= t2;
+
+    return *reinterpret_cast<float *>(&t1);
+}

Test/RayTracingTest.cpp
@@ -104,7 +104,7 @@ class camera {
 // Raytrace
 // Main
 int main(int argc, char** argv) {
-    // Image
+    // Render Settings
     const float_precision aspect_ratio = 16.0 / 9.0;
     const int image_width = 1920;
     const int image_height = static_cast<int>(image_width / aspect_ratio);

appveyor.yml
@@ -87,6 +87,9 @@ init:
       choco install vulkan-sdk -y
       refreshenv
 
+      Invoke-BatchFile "${env:ProgramFiles(x86)}\\Microsoft Visual Studio\\2022\\Community\\VC\\Auxiliary\\Build\\vcvarsall.bat" x86_amd64
+      Get-ChildItem env:* | Sort-Object name
+
 build_script:
 - cmd: >-
     scripts/build_imgui Release

commit a31546f798e6f23357ef0dcc76ff4148ef2e0a3f

fix bug introduced by 1b56d7287e92e1

Framework/Common/Image.hpp
@@ -119,7 +119,7 @@ struct Image {
         if (data) delete[] data;
     }
 
-    float GetR(uint32_t x, uint32_t y) const {
+    float GetX(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0;
 
         switch (pixel_format) {
@@ -151,7 +151,7 @@ struct Image {
         return 0;
     }
 
-    float GetG(uint32_t x, uint32_t y) const {
+    float GetY(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0;
 
         switch (pixel_format) {
@@ -187,7 +187,7 @@ struct Image {
         return 0;
     }
 
-    float GetB(uint32_t x, uint32_t y) const {
+    float GetZ(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0;
 
         switch (pixel_format) {
@@ -222,7 +222,7 @@ struct Image {
         return 0;
     }
 
-    float GetA(uint32_t x, uint32_t y) const {
+    float GetW(uint32_t x, uint32_t y) const {
         if (x >= Width || y >= Height) return 0xFF;
 
         switch (pixel_format) {
@@ -424,13 +424,13 @@ struct Image {
         return;
     }
 
-    uint8_t GetX(uint32_t x, uint32_t y) const { return GetR(x, y); }
+    uint8_t GetR(uint32_t x, uint32_t y) const { return to_unorm(GetX(x, y)); }
 
-    uint8_t GetY(uint32_t x, uint32_t y) const { return GetG(x, y); }
+    uint8_t GetG(uint32_t x, uint32_t y) const { return to_unorm(GetY(x, y)); }
 
-    uint8_t GetZ(uint32_t x, uint32_t y) const { return GetB(x, y); }
+    uint8_t GetB(uint32_t x, uint32_t y) const { return to_unorm(GetZ(x, y)); }
 
-    uint8_t GetW(uint32_t x, uint32_t y) const { return GetA(x, y); }
+    uint8_t GetA(uint32_t x, uint32_t y) const { return to_unorm(GetW(x, y)); }
 
     void SaveTGA(const char* filename) const {
         assert(filename != NULL);
@@ -467,10 +467,10 @@ struct Image {
         for (uint32_t y = 0; y < Height; y++) {
             for (uint32_t x = 0; x < Width; x++) {
                 // note reversed order: b, g, r
-                fputc(to_unorm(GetB(x, y)), file);
-                fputc(to_unorm(GetG(x, y)), file);
-                fputc(to_unorm(GetR(x, y)), file);
-                fputc(to_unorm(GetA(x, y)), file);
+                fputc(GetB(x, y), file);
+                fputc(GetG(x, y), file);
+                fputc(GetR(x, y), file);
+                fputc(GetA(x, y), file);
             }
         }
         fclose(file);

Utility/MaterialBaker.cpp
@@ -310,15 +310,15 @@ int main(int argc, char** argv) {
                 for (int32_t y = 0; y < surf.height; y++) {
                     for (int32_t x = 0; x < surf.width; x++) {
                         *(surf.ptr + y * surf.stride + x * channels) =
-                            metallic_texture->GetX(
+                            metallic_texture->GetR(
                                 std::floor(x * metallic_ratio_x),
                                 std::floor(y * metallic_ratio_y));
                         *(surf.ptr + y * surf.stride + x * channels + 1) =
-                            roughness_texture->GetX(
+                            roughness_texture->GetR(
                                 std::floor(x * roughness_ratio_x),
                                 std::floor(y * roughness_ratio_y));
                         *(surf.ptr + y * surf.stride + x * channels + 2) =
-                            ao_texture->GetX(std::floor(x * ao_ratio_x),
+                            ao_texture->GetR(std::floor(x * ao_ratio_x),
                                              std::floor(y * ao_ratio_y));
                         *(surf.ptr + y * surf.stride + x * channels + 3) = 0;
                     }
@@ -351,9 +351,9 @@ int main(int argc, char** argv) {
                 for (int32_t y = 0; y < surf.height; y++) {
                     for (int32_t x = 0; x < surf.width; x++) {
                         *(surf.ptr + y * surf.stride + x * channels) =
-                            normal_texture->GetX(x, y);
+                            normal_texture->GetR(x, y);
                         *(surf.ptr + y * surf.stride + x * channels + 1) =
-                            normal_texture->GetY(x, y);
+                            normal_texture->GetG(x, y);
                     }
                 }
 