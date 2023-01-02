777c1ba0c13f2992bb4666b8206d440e1423a533
netwarm007 <chenwenli@chenwenli.com>
2023-01-02 14:05:40 +0800

    add new batch file

---
+++ scripts/build-vs.bat
@@ -0,0 +1,6 @@
+mkdir build
+pushd build
+cmake -G "Visual Studio 17 2022" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DOPENGL_RHI_DEBUG=ON -DD3D12_RHI_DEBUG=ON ..
+if "%1" == "" (cmake --build . --config Debug) else (cmake --build . --config %1)
+popd
+
