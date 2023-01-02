b781557fe85af25fac92a43b52aa165818dec8bb
netwarm007 <chenwenli@chenwenli.com>
2023-01-02 23:42:40 +0800

    adjust CPU raytracing sample settings to match CUDA

--- Test/RayTracingTest.cpp
+++ Test/RayTracingTest.cpp
@@ -69,7 +69,7 @@ int main(int argc, char** argv) {
     const float_precision aspect_ratio = 16.0 / 9.0;
     const int image_width = 1920;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 500;
+    const int samples_per_pixel = 512;
     const int max_depth = 50;
 
     // World
